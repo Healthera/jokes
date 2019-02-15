e5ad0bea51060d4c1e4df6bd2748a23d5a6b62d9
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-15 10:18:06 +0000

    set up future components in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,25 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/NavBar.js";
+import Index from "./components/Index.js";
+import JokesList from "./components/JokesList.js";
+import CreateJoke from "./components/CreateJoke.js";
+import EditJoke from "./components/EditJoke.js";
+import DeleteJoke from "./components/DeleteJoke.js";
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          // add navbar
           <NavBar/>
-
-          // add path '/'
-
-          //add path '/jokeslist'
-
-          //add path '/create'
-
-          //add path '/edit/:id'
-
-          //add path '/delete/:id'
-
+          <br/>
+          <Route path="/" exact component={Index} />
+          <Route path="/jokesList" component={JokesList} />
+          <Route path="/create" component={CreateJoke} />
+          <Route path="/edit/:id" component={EditJoke} />
+          <Route path="/delete/:id" component={DeleteJoke} />
         </div>
       </Router>
     );
