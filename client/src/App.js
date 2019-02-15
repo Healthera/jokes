791426c5f02e4d220bd078479d1c47d323977d32
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-15 09:31:44 +0000

    set up client folder and created react app

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        // add navbar
+
+        // add path '/'
+
+        //add path '/jokeslist'
+
+        //add path '/create'
+
+        //add path '/edit/:id'
+
+        //add path '/delete/:id'
+        
+      </div>
+    );
+  }
+}
+
+export default App;
