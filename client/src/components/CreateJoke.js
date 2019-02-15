cfa67eb2559e237390bf975800420779129eae3a
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-15 10:29:34 +0000

    added basic create joke render

---
+++ client/src/components/CreateJoke.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import axios from 'axios';
+
+class CreateJoke extends Component {
+  render(){
+    return(
+      <div style={{marginTop: 10}}>
+        <h3>Create New Joke</h3>
+      </div>
+    )
+  }
+}
+export default CreateJoke;
