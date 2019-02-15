274f2ffe5bbebd8c4994e11ffb1cc7d717ae53ad
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-15 09:55:11 +0000

    added logo with healthera redirect

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "../assets/healthera.ico"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class NavBar extends Component {
+  render(){
+    return(
+      <nav class="navbar navbar-expand-lg navbar-light bg-success">
+        <a class="navbar-brand" href="https://healthera.co.uk/" target="_blank" rel="noopener noreferrer">
+          <img src={logo} width="30" height="30" alt="Healthera" />
+        </a>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
