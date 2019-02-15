ffa7341a98fb2b33bf379a1c03517fc62fc7eff6
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-15 10:22:07 +0000

    added basic index code to index so app doesnt crash

---
+++ client/src/components/Index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Index extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Index;
