ea826aca0ca9dd2d52e3a7b58186eea3c7e9e166
djstephenson7 <dan_0502@hotmail.co.uk>
2019-02-18 12:19:37 +0000

    Tested for new joke error

--- test/joke.controller.test.js
+++ test/joke.controller.test.js
@@ -33,9 +33,21 @@ describe('Get all Jokes', () => {
   });
 });
 
-describe('Post a new joke', () => {
-  it('Should post a new joke', (done) => {
-    const MockJoke = sinon.mock(new Joke({ joke: 'Mock joke' }));
+describe('Post a new Joke', () => {
+  it('Should create a new joke', (done) => {
+    const MockJoke = sinon.mock(new Joke({ joke: 'New Mock joke' }));
+    const joke = MockJoke.object;
+    const expectedResult = { status: false };
+    MockJoke.expects('save').yields(expectedResult, null);
+    joke.save((err, result) => {
+      MockJoke.verify();
+      MockJoke.restore();
+      expect(err.status).to.not.be.true;
+      done();
+    });
+  });
+  it('Should return an error if the joke fails to save', (done) => {
+    const MockJoke = sinon.mock(new Joke({ joke: 'Save new Joke from mock' }));
     const joke = MockJoke.object;
     const expectedResult = { status: false };
     MockJoke.expects('save').yields(expectedResult, null);
