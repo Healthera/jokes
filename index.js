1b0010f26dd0398b475bd75435d5878ac242f105
djstephenson7 <dan_0502@hotmail.co.uk>
2019-02-18 11:07:00 +0000

    Added bodyParser to index.js

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const fs = require('fs');
 
 const app = express();
 const parse = require('csv-parse');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const apiRoutes = require('./app/routes/api-routes')
+const apiRoutes = require('./app/routes/api-routes');
 
-app.use('/api', apiRoutes)
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(bodyParser.json());
+
+app.use('/api', apiRoutes);
+
+const db = mongoose.connection;
 
 fs.readFile('jokes.csv', (err, data) => {
   parse(data, {}, (err, jokes) => {
