152dabbc3232189c7c2c4adb5a613124c7603b3d
djstephenson7 <dan_0502@hotmail.co.uk>
2019-02-18 10:43:49 +0000

    Installed dependencies & Eslint

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
 const parse = require('csv-parse');
 
 fs.readFile('jokes.csv', (err, data) => {
   parse(data, {}, (err, jokes) => {
-    app.get('/', function (req, res) {
-      let i = Math.floor((Math.random() * jokes.length));
-      let response = {
-        joke: jokes[i][0]
-      }
+    app.get('/', (req, res) => {
+      const i = Math.floor((Math.random() * jokes.length));
+      const response = {
+        joke: jokes[i][0],
+      };
       res.json(response);
     });
   });
 });
 
-app.listen(3050, function () {
+app.listen(3050, () => {
   console.log('Example app listening on port 3050.');
 });
