f21f3a73839183b5cd784fc2d794c17af9377a71
djstephenson7 <dan_0502@hotmail.co.uk>
2019-02-18 10:55:15 +0000

    API routes working in app

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const app = express();
 const parse = require('csv-parse');
 
+const apiRoutes = require('./app/routes/api-routes')
+
+app.use('/api', apiRoutes)
+
 fs.readFile('jokes.csv', (err, data) => {
   parse(data, {}, (err, jokes) => {
     app.get('/', (req, res) => {
