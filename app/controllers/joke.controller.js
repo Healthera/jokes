d06a49c3d1c0659f5d979af38650c471f12d2d21
djstephenson7 <dan_0502@hotmail.co.uk>
2019-02-18 11:59:03 +0000

    Add new Joke function in Controller

--- app/controllers/joke.controller.js
+++ app/controllers/joke.controller.js
@@ -15,3 +15,17 @@ exports.index = (req, res) => {
     });
   });
 };
+
+exports.new = (req, res) => {
+  const joke = new Joke();
+  // save the joke and check for errors
+  joke.save((err) => {
+    if (err) {
+      res.json(err);
+    }
+    res.json({
+      message: 'New joke created!',
+      data: joke,
+    });
+  });
+};
--- test/joke.controller.test.js
+++ test/joke.controller.test.js
@@ -21,14 +21,29 @@ describe('Get all Jokes', () => {
     });
   });
   it('Should return an error', (done) => {
-    const MockJoke = sinon.mock(Joke)
-    const expectedResult = { status: false, error: 'Something went wrong' }
+    const MockJoke = sinon.mock(Joke);
+    const expectedResult = { status: false, error: 'Something went wrong' };
     MockJoke.expects('find').yields(null, expectedResult);
     Joke.find((err, result) => {
       MockJoke.verify();
       MockJoke.restore();
-      expect(result.status).not.to.be.true
+      expect(result.status).not.to.be.true;
       done();
-    })
-  })
+    });
+  });
+});
+
+describe('Post a new joke', () => {
+  it('Should post a new joke', (done) => {
+    const MockJoke = sinon.mock(new Joke({ joke: 'Mock joke' }));
+    const joke = MockJoke.object;
+    const expectedResult = { status: false };
+    MockJoke.expects('save').yields(expectedResult, null);
+    joke.save((err, result) => {
+      MockJoke.verify();
+      MockJoke.restore();
+      expect(err.status).to.not.be.true;
+      done();
+    });
+  });
 });
