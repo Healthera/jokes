a37cf37fef499c305c78bc015bf68eca4a3db3e3
notsag-dev <a.gaston.gimenez@gmail.com>
2019-02-24 20:16:31 -0300

    Remove line

--- api/config/database.js
+++ api/config/database.js
@@ -2,6 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://db:27017/jokesDb', { useNewUrlParser: true });
 
-
 const jokeSchema = new mongoose.Schema({ text: String });
 module.exports.Joke = mongoose.model('Joke', jokeSchema);
